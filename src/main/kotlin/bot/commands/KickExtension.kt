82bf835914f6af06a028fa5c37cce2bc07eee186
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-12-11 15:08:45 -0800

    fix(Kick): perm check

--- src/main/kotlin/bot/commands/KickExtension.kt
+++ src/main/kotlin/bot/commands/KickExtension.kt
@@ -4,6 +4,7 @@ package bot.commands
 
 import bot.WARNING
 import com.kotlindiscord.kord.extensions.checks.anyGuild
+import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalString
 import com.kotlindiscord.kord.extensions.commands.converters.impl.user
@@ -11,6 +12,7 @@ import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.common.Color
+import dev.kord.common.entity.Permission
 import dev.kord.rest.Image
 import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.Clock
@@ -22,7 +24,12 @@ class KickExtension : Extension() {
         publicSlashCommand(::KickArgs) {
             name = this@KickExtension.name
             description = "Kick a specific user"
-            check { anyGuild() }
+            requireBotPermissions(Permission.KickMembers)
+
+            check {
+                anyGuild()
+                hasPermission(Permission.KickMembers)
+            }
 
             action {
                 val reason = arguments.reason ?: "None"
