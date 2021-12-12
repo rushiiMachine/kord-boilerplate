25cccb7e7d8b86f3e300889229456970bfb864cc
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-12-11 20:12:14 -0800

    feat(purge): top authors + fix error

--- src/main/kotlin/bot/commands/PurgeExtension.kt
+++ src/main/kotlin/bot/commands/PurgeExtension.kt
@@ -6,11 +6,11 @@ import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingInt
 import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalString
 import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
+import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
-import com.kotlindiscord.kord.extensions.utils.delete
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Snowflake
+import dev.kord.core.kordLogger
 import dev.kord.rest.json.request.BulkDeleteRequest
 import kotlinx.coroutines.flow.toList
 import kotlinx.datetime.Clock
@@ -22,14 +22,15 @@ class PurgeExtension : Extension() {
     override val name = "purge"
 
     override suspend fun setup() {
-        publicSlashCommand(::BanArgs) {
+        ephemeralSlashCommand(::BanArgs) {
             name = this@PurgeExtension.name
             description = "Purge"
-            requireBotPermissions(Permission.ManageMessages, Permission.ReadMessageHistory, Permission.ViewChannel)
+            requireBotPermissions(Permission.ManageMessages, Permission.ReadMessageHistory)
 
             check {
                 anyGuild()
                 hasPermission(Permission.ManageMessages)
+                // TODO: prevent purging less than 2
             }
 
             action {
@@ -38,28 +39,36 @@ class PurgeExtension : Extension() {
 
                 val limit = Clock.System.now() - 14.days
                 val (messages, oldMessages) = channel.getMessagesBefore(Snowflake.max, arguments.count).toList()
-                    .map { it.id }
                     .partition { it.timestamp > limit }
 
                 channel.kord.rest.channel.bulkDelete(channel.id,
-                    BulkDeleteRequest(messages),
-                    "Purge by ${author.tag} (${author.id}) with reason: $providedReason"
-                )
+                    BulkDeleteRequest(messages.map { it.id }),
+                    "Purge by ${author.tag} (${author.id}) with reason: $providedReason")
 
-                val response = respond {
-                    content = "Purged ${messages.size} messages."
+                val authors = messages // Message[]
+                    .mapNotNull { it.author?.id?.value } // Snowflake[] message author ids
+                    .groupBy { it } // Map<Snowflake, Snowflake[] all instances of that snowflake>
+                    .entries.associateBy({ it.value.size }) { it.key } // Map<instances count, Snowflake>
+                    .toSortedMap(Comparator.reverseOrder()) // Sort by instance count
+                    .entries.take(10) // Take top 10
+
+                respond {
+                    content = "Purged ${messages.size} total messages."
                     if (arguments.count > 100)
                         content += " *Limited to 100 messages.*"
                     if (oldMessages.isNotEmpty())
                         content += "\n*This purge targeted messages older than 14 days which were ignored.*"
+                    if (authors.isNotEmpty()) {
+                        content += "\n\nTop message authors:\n"
+                        content += authors.joinToString("\n") { "<@${it.value}>: ${it.key} messages" }
+                    }
                 }
-                response.delete(5000, true)
             }
         }
     }
 
     inner class BanArgs : Arguments() {
-        val count by defaultingInt("count", "Number of messages to delete (Max 100, default 5)", 5)
+        val count by defaultingInt("count", "Number of messages to delete (Range: 2-100)", 5)
         val reason by optionalString("reason", "Purge reason")
     }
 }
