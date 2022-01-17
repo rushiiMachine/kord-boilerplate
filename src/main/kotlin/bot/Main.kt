c66d44f82154e315b1acff1b71f54365ecf3765d
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-01-16 19:43:18 -0800

    feat: limit caches

--- src/main/kotlin/bot/Main.kt
+++ src/main/kotlin/bot/Main.kt
@@ -5,13 +5,15 @@ import bot.database.RoleButtonRecord
 import bot.events.MemberLogExtension
 import bot.events.ReadyExtension
 import com.kotlindiscord.kord.extensions.ExtensibleBot
+import dev.kord.cache.map.MapLikeCollection
+import dev.kord.cache.map.internal.MapEntryCache
+import dev.kord.cache.map.lruLinkedHashMap
 import dev.kord.core.kordLogger
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
 import io.github.cdimascio.dotenv.dotenv
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
 import kotlin.system.exitProcess
@@ -60,7 +62,25 @@ suspend fun main() {
             add(::ReadyExtension)
         }
 
-        // TODO: fix cache
+        kord {
+            cache {
+                users { cache, description ->
+                    MapEntryCache(cache, description, MapLikeCollection.lruLinkedHashMap(maxSize = 50))
+                }
+                members { cache, description ->
+                    MapEntryCache(cache, description, MapLikeCollection.lruLinkedHashMap(maxSize = 50))
+                }
+                emojis { cache, description ->
+                    MapEntryCache(cache, description, MapLikeCollection.none())
+                }
+                channels { cache, description ->
+                    MapEntryCache(cache, description, MapLikeCollection.lruLinkedHashMap(maxSize = 200))
+                }
+                roles { cache, description ->
+                    MapEntryCache(cache, description, MapLikeCollection.lruLinkedHashMap(maxSize = 50))
+                }
+            }
+        }
     }
 
     Runtime.getRuntime().addShutdownHook(Thread {
